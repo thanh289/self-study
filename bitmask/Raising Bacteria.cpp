cf0a56fd0af0d96206b217b1f4b914a2000825d3
thanh289 <87253453+thanh289@users.noreply.github.com>
2021-11-21 10:34:01 +0700

    Create Raising Bacteria.cpp

---
+++ bitmask/Raising Bacteria.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main()
+{
+	int n; cin>>n;
+	//__builtin_popcount(): check how many bit 1
+	cout<<__builtin_popcount(n);
+}
